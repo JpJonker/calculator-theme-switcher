0cf24b5149bc8f0eafabb4fc033616742c9a3949
JpJonker <jpjonker.business@gmail.com>
2023-02-09 12:13:25 +0200

    refactored the calculator logic

--- calculate.js
+++ calculate.js
@@ -1,120 +1,91 @@
-var firstValue;
-var secondValue;
-var operationToUse;
+var firstNum;
+var secondNum;
+var operator;
 
-$(".num0").click(function() {
-    $(".user-input-text").append("0");
+for (let i = 0; i < 10; i++) {
+    $(`.num${i}`).click(function() {
+        $('.input-display').append(i);
+    })
+}
+
+$('.dot').click(function() {
+    $('.input-display').append('.')
 })
 
-$(".num1").click(function() {
-    $(".user-input-text").append("1");
-})
-
-$(".num2").click(function() {
-    $(".user-input-text").append("2");
-})
-
-$(".num3").click(function() {
-    $(".user-input-text").append("3");
-})
-
-$(".num4").click(function() {
-    $(".user-input-text").append("4");
-})
-
-$(".num5").click(function() {
-    $(".user-input-text").append("5");
-})
-
-$(".num6").click(function() {
-    $(".user-input-text").append("6");
-})
-
-$(".num7").click(function() {
-    $(".user-input-text").append("7");
-})
-
-$(".num8").click(function() {
-    $(".user-input-text").append("8");
-})
-
-$(".num9").click(function() {
-    $(".user-input-text").append("9");
-})
-
-$(".comma").click(function() {
-    $(".user-input-text").append(".");
-})
-
-$(".delete").click(function() {
-    $(".user-input-text").text('');
-})
-
-$('.addition').click(function() {
-    operationToUse = '+';
-
-    if (!firstValue) {
-        firstValue = $('.user-input-text').text();
-        $('.user-input-text').text('');
+// refactor to more optimized later
+$('.add').click(function() {
+    if (!firstNum) {
+        firstNum = $('.input-display').text();
+        $('.input-display').text('');
     }
+
+    operator = 'add'
+})
+// refactor to more optimized later
+$('.subtract').click(function() {
+    if (!firstNum) {
+        firstNum = $('.input-display').text();
+        $('.input-display').text('');
+    }
+
+    operator = 'subtract'
+})
+// refactor to more optimized later
+$('.multiply').click(function() {
+    if (!firstNum) {
+        firstNum = $('.input-display').text();
+        $('.input-display').text('');
+    }
+
+    operator = 'multiply'
+})
+// refactor to more optimized later
+$('.divide').click(function() {
+    if (!firstNum) {
+        firstNum = $('.input-display').text();
+        $('.input-display').text('');
+    }
+
+    operator = 'divide';
 })
 
-$('.subtraction').click(function() {
-    operationToUse = '-';
-
-    if (!firstValue) {
-        firstValue = $('.user-input-text').text();
-        $('.user-input-text').text('');
-    }
-})
-
-$('.division').click(function() {
-    operationToUse = '/';
-
-    if (!firstValue) {
-        firstValue = $('.user-input-text').text();
-        $('.user-input-text').text('');
-    }
-})
-
-$('.multiplication').click(function() {
-    operationToUse = '*';
-
-    if (!firstValue) {
-        firstValue = $('.user-input-text').text();
-        $('.user-input-text').text('');
-    }
-})
-
-$('.equals').click(function() {
-    let result;
-    firstValue = firstValue;
-    secondValue = $('.user-input-text').text()
-
-    switch (operationToUse) {
-        case '+':
-            result = Number(firstValue) + Number(secondValue);
-            break;
-
-        case '-':
-            result = Number(firstValue) - Number(secondValue);
-            break;
-
-        case '/':
-            result = Number(firstValue) / Number(secondValue);
-            break;
-
-        case '*':
-            result = Number(firstValue) * Number(secondValue);
-            break;
-    }
-    
-    $('.user-input-text').text(result)
+$('.delete').click(function() {
+    $('.input-display').text('');
 })
 
 $('.reset').click(function() {
-    firstValue = '';
-    secondValue = '';
-    operationToUse = '';
-    $('.user-input-text').text('');
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+    $('.input-display').text('');
 })
+
+$('.equals').click(function(){
+    let result;
+    secondNum = $('.input-display').text()
+
+    switch (operator) {
+        case 'add': 
+            result = Number(firstNum) + Number(secondNum);
+            break;
+
+        case 'subtract': 
+            result = Number(firstNum) - Number(secondNum);
+            break;
+
+        case 'multiply': 
+            result = Number(firstNum) * Number(secondNum);
+            break;
+
+        case 'divide': 
+            result = Number(firstNum) / Number(secondNum);
+            break;
+    }
+
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+    
+    $('.input-display').text(result)
+})
+
